56ae22bc3c8b75fe90f7b730f40c091c530d7b64
ei <eivillamuelas@gmail.com>
2025-01-14 17:27:32 +0100

    a

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,19 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Versus';
+export const SITE_SUBTITLE = 'Universos a ambos lados';
 export const SITE_DESCRIPTION = 'Grupo de investigación - UDIT';
+export const COLORS = {
+    main:{
+        bg: "bg-green-200",
+        hv: "hover:bg-green-100",
+        br: "border border-green-300",
+        act: "bg-green-400"
+    },
+    alt:{
+        bg: "bg-blue-300",
+        hv: "bg-blue-200",
+        br: "border border-blue-300",
+        act: "bg-blue-400"
+    }
+}
