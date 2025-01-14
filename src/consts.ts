9fd5786ac05cabf7f297735b386fe37019b8b7c2
ei <eivillamuelas@gmail.com>
2025-01-14 12:48:01 +0100

    a

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Versus. Universos a ambos lados';
+export const SITE_TITLE = 'Versus';
 export const SITE_DESCRIPTION = 'Grupo de investigación - UDIT';
