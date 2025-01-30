1b64fdb945e0defb78a2f7b878f1d20f784e90a3
ei <eivillamuelas@gmail.com>
2025-01-30 20:46:11 +0100

    a

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'IAG y diseño';
-export const SITE_SUBTITLE = 'Tecnologías avanzadas para el diseño de espacios.';
+export const SITE_TITLE = 'Inteligencia artificial y diseño';
+export const SITE_SUBTITLE = 'Inteligencia artificial aplicada al diseño espacial';
 export const SITE_DESCRIPTION = 'Grupo de investigación Versus. Universos a ambos lados. UDIT';
 export const COLORS = {
     main:{
